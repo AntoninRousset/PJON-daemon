3d64126e7e1a676950801ebadf4363da4851611a
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2019-04-19 15:55:26 +0200

    client.c

--- server.c
+++ server.c
@@ -21,10 +21,10 @@ int open_socket(const char *filename)
   /* Create the socket. */
   sock = socket (PF_LOCAL, SOCK_STREAM, 0);
   if (sock < 0)
-    {
-      perror ("socket");
-      exit (EXIT_FAILURE);
-    }
+  {
+    perror ("socket");
+    exit (EXIT_FAILURE);
+  }
 
   /* Bind a name to the socket. */
   name.sun_family = AF_LOCAL;
@@ -34,10 +34,10 @@ int open_socket(const char *filename)
   size = SUN_LEN(&name);
 
   if (bind (sock, (struct sockaddr *) &name, size) < 0)
-    {
-      perror ("bind");
-      exit (EXIT_FAILURE);
-    }
+  {
+    perror ("bind");
+    exit (EXIT_FAILURE);
+  }
 
   return sock;
 }
@@ -49,20 +49,20 @@ int read_from_client (int filedes)
 
   nbytes = read (filedes, buffer, MAXMSG);
   if (nbytes < 0)
-    {
-      /* Read error. */
-      perror ("read");
-      exit (EXIT_FAILURE);
-    }
+  {
+    /* Read error. */
+    perror ("read");
+    exit (EXIT_FAILURE);
+  }
   else if (nbytes == 0)
     /* End-of-file. */
     return -1;
   else
-    {
-      /* Data read. */
-      fprintf (stderr, "Server: got message: `%s'\n", buffer);
-      return 0;
-    }
+  {
+    /* Data read. */
+    fprintf (stderr, "Server: got message: `%s'\n", buffer);
+    return 0;
+  }
 }
 
 int main()
@@ -77,59 +77,59 @@ int main()
   /* Create the socket and set it up to accept connections. */
   sock = open_socket("/tmp/pjon.sock");
   if (listen (sock, 1) < 0)
-    {
-      perror ("listen");
-      exit (EXIT_FAILURE);
-    }
+  {
+    perror ("listen");
+    exit (EXIT_FAILURE);
+  }
 
   /* Initialize the set of active sockets. */
   FD_ZERO (&active_fd_set);
   FD_SET (sock, &active_fd_set);
 
   while (1)
+  {
+    /* Block until input arrives on one or more active sockets. */
+    read_fd_set = active_fd_set;
+    if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
     {
-      /* Block until input arrives on one or more active sockets. */
-      read_fd_set = active_fd_set;
-      if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
-        {
-          perror ("select");
-          exit (EXIT_FAILURE);
-        }
-
-      /* Service all the sockets with input pending. */
-      for (i = 0; i < FD_SETSIZE; ++i)
-        if (FD_ISSET (i, &read_fd_set))
-          {
-            if (i == sock)
-              {
-                /* Connection request on original socket. */
-                int new;
-                size = sizeof (clientname);
-                new = accept (sock,
-                              (struct sockaddr *) &clientname,
-                              &size);
-                if (new < 0)
-                  {
-                    perror ("accept");
-                    exit (EXIT_FAILURE);
-                  }
-                fprintf (stderr,
-                         "Server: connect from host %s, port %hd.\n",
-                         inet_ntoa (clientname.sin_addr),
-                         ntohs (clientname.sin_port));
-                FD_SET (new, &active_fd_set);
-              }
-            else
-              {
-                /* Data arriving on an already-connected socket. */
-                if (read_from_client (i) < 0)
-                  {
-                    close (i);
-                    FD_CLR (i, &active_fd_set);
-                  }
-              }
-          }
+      perror ("select");
+      exit (EXIT_FAILURE);
     }
+
+    /* Service all the sockets with input pending. */
+    for (i = 0; i < FD_SETSIZE; ++i)
+      if (FD_ISSET (i, &read_fd_set))
+      {
+        if (i == sock)
+        {
+          /* Connection request on original socket. */
+          int new;
+          size = sizeof (clientname);
+          new = accept (sock,
+              (struct sockaddr *) &clientname,
+              &size);
+          if (new < 0)
+          {
+            perror ("accept");
+            exit (EXIT_FAILURE);
+          }
+          fprintf (stderr,
+              "Server: connect from host %s, port %hd.\n",
+              inet_ntoa (clientname.sin_addr),
+              ntohs (clientname.sin_port));
+          FD_SET (new, &active_fd_set);
+        }
+        else
+        {
+          /* Data arriving on an already-connected socket. */
+          if (read_from_client (i) < 0)
+          {
+            close (i);
+            FD_CLR (i, &active_fd_set);
+          }
+        }
+      }
+  }
 }
 
 
